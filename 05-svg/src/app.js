9f337fdc9bb2c8b631392fec0a620811b5d31d83
David Daxbacher <ddaxbacher.mmt-b2015@fh-salzburg.ac.at>
2017-01-03 17:55:50 +0100

    added interactive button

--- 05-svg/src/app.js
+++ 05-svg/src/app.js
@@ -7,6 +7,8 @@ import textTpl from './templates/text.hbs'
 import interactionTpl from './templates/interaction.hbs'
 import notFoundTpl from './templates/not-found.hbs'
 
+import $ from 'jquery'
+
 const app = document.getElementById('app')
 
 function index() {
@@ -31,6 +33,45 @@ function text() {
 
 function interaction() {
   app.innerHTML = interactionTpl()
+
+  let currentX = 0
+  let widthDragButton = $(".button-draggable").attr('width')
+  let widthViewport = $(".commit-button").attr('width')
+  let widthButton = parseInt($(".button-background").attr('width'))
+  let currentButtonWidth = 0
+  let ratio = widthViewport/widthButton
+  let isDragging = false
+
+  $(".button-draggable").on('mousedown', e => {
+      currentX = e.clientX
+      isDragging = true
+  });
+
+  $(".button-draggable").on('mousemove', e => {
+      if(!isDragging)
+        return
+
+      let dx = parseInt(e.clientX - currentX)
+      currentButtonWidth = parseInt(widthDragButton) + parseInt(dx/ratio)
+
+      if(currentButtonWidth >= widthDragButton && currentButtonWidth <= widthButton) {
+        $(".button-draggable").attr('width', currentButtonWidth)
+        $(".button-arrow").attr('transform', `translate(${currentButtonWidth - widthDragButton},0)`)
+      }
+  });
+
+  $(".button-draggable").on('mouseup', e => {
+      isDragging = false
+      if(currentButtonWidth >= widthButton) { //update button state
+        $(".text-commited").removeClass("hidden")
+        $(".button-draggable").removeClass()
+        $(".button-arrow").removeClass()
+      }
+      else {  //reset button state
+        $(".button-draggable").attr('width', widthDragButton)
+        $(".button-arrow").attr('transform', 'translate(0,0)')
+      }
+  });
 }
 
 function notFound() {
